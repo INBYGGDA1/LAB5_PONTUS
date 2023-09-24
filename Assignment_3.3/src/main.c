990f602c280a80b629c6e42dfcdd9f3eb8aee84e
pontus <github@doughnut.mailer.me>
2023-09-24 20:56:24 +0200

    Added folders for lab5

---
+++ Assignment_3.3/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * ================================================================
+ * File: main.c
+ * Author: Pontus Svensson, Carl Larsson
+ * Email: psn19003@student.mdu.se, cln20001@student.mdu.se
+ * Date: 2023-09-24
+ * Description:  
+ *
+ * License: This code is distributed under the MIT License. visit
+ * https://opensource.org/licenses/MIT for more information.
+ * ================================================================
+ */
+
+#include <stdlib.h>
+#include "../inc/FreeRTOSConfig.h"
+int main(void) { return EXIT_SUCCESS; }
--- src/main.c
+++
@@ -1,3 +0,0 @@
-#include <stdlib.h>
-#include "../inc/FreeRTOSConfig.h"
-int main(void) { return EXIT_SUCCESS; }
