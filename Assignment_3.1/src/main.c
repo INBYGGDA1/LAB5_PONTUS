0d30159f7fd152cd306f52a348eed30cca359d0a
pontus <github@doughnut.mailer.me>
2023-09-27 21:15:34 +0200

    something

--- Assignment_3.1/src/main.c
+++ Assignment_3.1/src/main.c
@@ -11,28 +11,59 @@
  * ================================================================
  */
 
-#include <stdlib.h>
-#include <stdint.h>
+/*================================================================*/
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+/* TIWARE */
 #include "driverlib/sysctl.h"
-#include "drivers/pinout.h"
-#include "driverlib/pin_map.h"
-#include "drivers/buttons.h"
-#include "../inc/FreeRTOSConfig.h"
-#include "FreeRTOS.h"
-#include "inc/hw_memmap.h"
-#include "task.h"
 #include "driverlib/gpio.h"
 #include "driverlib/uart.h"
+#include "driverlib/pin_map.h"
+
+#include "drivers/buttons.h"
+#include "drivers/pinout.h"
+
+#include "inc/hw_memmap.h"
+#include "inc/hw_ints.h"
+
 #include "utils/uartstdio.h"
 
-void delay(uint32_t number_of_loops) {
-  volatile uint32_t i;
-  for (i = 0; i < number_of_loops; i++) {
-    // Just looping for delay
-  }
-}
+/* FreeRTOS */
+#include "../inc/FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "portable.h"
+#include "portmacro.h"
+#include "projdefs.h"
+#include "task.h"
 
+/*================================================================*/
+struct xParam {
+  uint32_t systemClock;
+  uint32_t xLed;
+  uint32_t xButton;
+};
+
+/*================================================================*/
+volatile uint8_t led_array[] = {CLP_D1, CLP_D2, CLP_D3, CLP_D4};
+volatile uint8_t button_array[] = {LEFT_BUTTON, RIGHT_BUTTON};
+TaskHandle_t xHandle_LED[4];
+
+/*================================================================*/
+/* The error routine that is called if the driver library         */
+/* encounters an error.                                           */
+/*================================================================*/
+#ifdef DEBUG
+void __error__(char *pcFilename, uint32_t ui32Line) {
+  while (1)
+    ;
+}
+#endif
+
+/*================================================================*/
+/*            Helper function configure uart                      */
+/*================================================================*/
 void ConfigureUART() {
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
   SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
@@ -43,45 +74,126 @@ void ConfigureUART() {
   UARTStdioConfig(0, 115200, 16000000);
 }
 
-void vLedBlinker() {
+/*================================================================*/
+/*           Task function to blink the LEDs                      */
+/*================================================================*/
+void vLedBlinker(void *pvParameters) {
+  struct xParam *param = (struct xParam *)
+      pvParameters; // cast conversion of the parameters to the correct format
+  const TickType_t xDelay = ((param->xLed + 1) * 1000) /
+                            portTICK_PERIOD_MS; // Converting from MS to S
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  xLastWakeTime = xTaskGetTickCount();
+  while (1) {
+    LEDWrite(led_array[param->xLed], led_array[param->xLed]);
+    // UARTprintf("LED [%d] ON\n", param->xLed);
+    vTaskDelayUntil(&xLastWakeTime, xDelay);
+    LEDWrite(led_array[param->xLed], 0);
+    // UARTprintf("LED [%d] OFF\n", param->xLed);
+    vTaskDelayUntil(&xLastWakeTime, xDelay);
+  }
+  vPortFree(param);
+}
+
+/*================================================================*/
+/*           Task to check for button presses                     */
+/*================================================================*/
+void vButtonPress(void *pvParameters) {
   unsigned char ucDelta, ucState;
-  volatile uint32_t t = 50000;
-  uint8_t led_tracker = 0;
-  uint8_t led_array[] = {CLP_D1, CLP_D2, CLP_D3, CLP_D4};
+  struct xParam *param = (struct xParam *)pvParameters;
+  const TickType_t xDelay = (10 * 1000) / portTICK_RATE_MS; // 10 second delay
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  xLastWakeTime = xTaskGetTickCount();
+
   while (1) {
     ucState = ButtonsPoll(&ucDelta, 0);
-    // Loop forward from D1 -> D4
-    for (led_tracker = 0; led_tracker < 4; led_tracker++) {
-      LEDWrite(led_array[led_tracker], led_array[led_tracker]);
-      delay(t);
-      LEDWrite(led_array[led_tracker], 0);
-    }
-    // A LEDs are off before going backwards
-    // We start at LED D3, Then D2.
-    // To stop same led light up twice we dont need to light D1,
-    // It is enough to just restart from beginning with a delay at the end
-    for (led_tracker = 2; led_tracker > 0; led_tracker--) {
-      LEDWrite(led_array[led_tracker], led_array[led_tracker]);
-      delay(t);
-      LEDWrite(led_array[led_tracker], 0);
-    }
-    delay(t);
-  }
-}
-void vButtonPress() {
-  while (1) {
-  }
-}
-int main(void) {
 
-  ConfigureUART();
+    if (BUTTON_PRESSED(button_array[param->xButton], ucState, ucDelta) ||
+        BUTTON_RELEASED(button_array[param->xButton], ucState,
+                        ucDelta)) { // Check for button presses
 
+      vTaskSuspend(xHandle_LED[param->xLed]);  // vTaskSuspend, suspends anys.
+                                               // Whenever a task is suspended
+      UARTprintf("Button pressed: %d\n", button_array[param->xButton]);
+      LEDWrite(led_array[param->xLed], led_array[param->xLed]);
+      vTaskDelayUntil(&xLastWakeTime, xDelay);
+      LEDWrite(led_array[param->xLed], 0);
+      vTaskResume(xHandle_LED[param->xLed]);
+    }
+    vTaskDelayUntil(&xLastWakeTime, (10 / portTICK_RATE_MS));
+  }
+  vPortFree(param);
+}
+
+void GPIO_init() {
   PinoutSet(false, false);
   ButtonsInit();
-
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+  while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)) {
+    ;
+  }
+  while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)) {
+    ;
+  }
   GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, CLP_D1_PIN | CLP_D2_PIN);
   GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, CLP_D3_PIN | CLP_D4_PIN);
+}
+/*================================================================*/
+/*                          Main                                  */
+/*================================================================*/
+int main(void) {
+  uint32_t systemClock;
+  uint32_t i, j;
 
-  xTaskCreate(vLedBlinker, "TaskLEDBlink", 100, NULL, 1, NULL);
-  xTaskCreate(vButtonPress, "TaskButtonPress", 100, NULL, 2, NULL);
+  BaseType_t xButtonReturned; // types used to check whether the task was
+                              // created succesfully
+  BaseType_t xLedReturned;
+  TaskHandle_t xHandle_BUTTON = NULL;
+
+  struct xParam *param; // Struct to use as parameters to the tasks
+  systemClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN |
+                                    SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480),
+                                   120000000);
+
+  ConfigureUART(); // Enable uart
+  GPIO_init();     // Enable the pins for the LEDs and buttons
+
+  for (i = 0; i < 4; i++) { // Create 4 tasks, each task controls 1 LED
+    param = pvPortMalloc(
+        sizeof(struct xParam)); // Allocate memory for each task parameters
+    if (param == NULL) {
+      break;
+    }
+    param->systemClock = systemClock;
+    param->xLed =
+        i; // xLed is used to toggle the correct LED defined in the led_array
+    param->xButton = 0; // not used in vLedBlinker
+    UARTprintf("vLedTASK: %d, systemClock = %d, xLed = %d\n", i + 1,
+               param->systemClock, param->xLed);
+    xLedReturned = xTaskCreate(vLedBlinker, "Blink", 100, (void *)param, 1,
+                               &xHandle_LED[i]);
+
+    if (xLedReturned == pdPASS) {
+      UARTprintf("vLedTASK: %d, created\n", i + 1);
+    }
+  }
+
+  for (j = 0; j < 2; j++) { // Create 2 tasks which controls LEFT and Right
+                            // button respectively
+    param = pvPortMalloc(sizeof(struct xParam));
+    if (param == NULL) {
+      break;
+    }
+    param->systemClock = systemClock;
+    param->xButton = j;
+    param->xLed = j + 1; // will toggle the leds in the middle
+    xButtonReturned = xTaskCreate(vButtonPress, "Button", 100, (void *)param, 2,
+                                  &xHandle_BUTTON);
+    if (xButtonReturned == pdPASS) {
+      UARTprintf("vButtonTask: %d created\n", j + 1);
+    }
+  }
+
+  vTaskStartScheduler();
 }
